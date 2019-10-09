6cdf42c1f6ccb73490413a530adab9d874682731
rktel <rakinakuy@hotmail.com>
2019-10-09 01:55:53 -0500

    m3

--- main.js
+++ main.js
@@ -8,9 +8,18 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("CALL SP_INSERT_DATA ('ABC-584','-12.98767','-76.464646','360','67','RU', '2019-10-08 12:00:00', '2019-10-08 12:01:00');", function (error, results, fields) {
+connection.query('SELECT * FROM data', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results);
 });
 
 connection.end();
+
+/**
+ connection.query("CALL SP_INSERT_DATA ('ABC-584','-12.98767','-76.464646','360','67','RU', '2019-10-08 12:00:00', '2019-10-08 12:01:00');", function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+}); 
+  
+ 
+*/
