c6bf1ce55fbce115450e6f6283d20c070d0a60ce
rktel <rakinakuy@hotmail.com>
2019-10-10 22:18:53 -0500

    function insert

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
-var mysql      = require('mysql');
+
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'george',
-  password : 'pig',
-  database : 'gps'
+  host: 'localhost',
+  user: 'george',
+  password: 'pig',
+  database: 'gps'
 });
 
 const data = {
-    placa: "KIS-925",
-    latitud:"-19.9999",
-    longitud:"-66.5555",
-    rumbo:"91",
-    velocidad:"80",
-    evento:"AT",
-    fecha:"2019-12-07 12:45:00",
-    fechaemv:"2019-09-08 12:00:22"
+  placa: "KIS-925",
+  latitud: "-19.9999",
+  longitud: "-66.5555",
+  rumbo: "91",
+  velocidad: "80",
+  evento: "AT",
+  fecha: "2019-12-07 12:45:00",
+  fechaemv: "2019-09-08 12:00:22"
 }
 
-connection.connect();
-
-connection.query('CALL SP_INSERT_DATA (?,?,?,?,?,?,?,?);',[data.placa,data.latitud, data.longitud, data.rumbo, data.velocidad, data.evento, data.fecha, data.fechaemv], function (error, results, fields) {
+function insertData() {
+  connection.connect();
+  connection.query('CALL SP_INSERT_DATA (?,?,?,?,?,?,?,?);', [data.placa, data.latitud, data.longitud, data.rumbo, data.velocidad, data.evento, data.fecha, data.fechaemv], function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results);
-  }); 
-    
+  });
+  connection.end();
+}
 
 
-connection.end();
+
+
+
+
 
 /**
  connection.query("CALL SP_INSERT_DATA ('ABC-584','-12.98767','-76.464646','360','67','RU', '2019-10-08 12:00:00', '2019-10-08 12:01:00');", function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results);
-}); 
-  
+});
+
 connection.query('SELECT * FROM data', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results);
-}); 
+});
 
 connection.query('CALL SP_INSERT_DATA (`${data.placa}`,${data.latitud},${data.longitud},${data.rumbo},${data.velocidad},${data.evento},${data.fecha},${data.fechaemv});', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results);
-  }); 
-    
+  });
+
 
 */
